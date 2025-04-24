a5ac9aa10a5b57b5b1c6daf8a2b8c4d69915eeed
Noah Villa <142937850+Noah13579@users.noreply.github.com>
2025-04-24 10:26:00 -0500

    Basic motor functionality

--- finalproj.py
+++ finalproj.py
@@ -1,11 +1,18 @@
 import cv2
 import numpy as np
+import RPi.GPIO as GPIO
+import time
 
 '''
 The following function is inspired by:
     User raja_961, "Autonomous Lane-Keeping Car Using Raspberry Pi and OpenCV". 
     Instructables. URL: https://www.instructables.com/Autonomous-Lane-Keeping-Car-Using-Raspberry-Pi-and/
 '''
+
+SPEED_PIN = 18
+STEERING_PIN = 19
+
+freq = 50
 
 def start_video():
     '''
@@ -92,3 +99,49 @@ def detect_red_pix(hsv):
         return True
     else:
         return False
+
+def GPIO_setup():
+    """
+    Effects: Sets up the GPIO pins specified at the top of the file
+    """
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SPEED_PIN, GPIO.OUT)
+    GPIO.setup(STEERING_PIN, GPIO.OUT)
+
+def calibrate_esc(pwm):
+    """
+    Input:
+        - pwm: this should be our speed pwm
+    Effects:
+        - Calibrates the esc of the car
+    """
+    pwm.ChangeDutyCycle(10)
+    time.sleep(2)
+    pwm.ChangeDutyCycle(5)
+    time.sleep(2)
+    pwm.ChangeDutyCycle(7.5)
+    time.sleep(2)
+
+def set_servo_angle(angle, pwm):
+    """
+    Input: 
+        - pwm: a pwm object created by the GPIO library, should be the servo pin's pwm
+        - angle: the angle between 0 and 180 the servo should be at. Foward is 90 degrees
+    Effects:
+        - sets the servo to the requested angle
+    """
+    # Convert angle (0 to 180 degrees) to duty cycle (2.5% to 12.5%)
+    duty_cycle = 2.5 + (angle / 18)
+    pwm.ChangeDutyCycle(duty_cycle)
+    time.sleep(1)  # Allow time for the servo to reach the position
+
+def set_speed(pwm, speed_percent):
+    """
+    Input: 
+        - pwm: a pwm object created by the GPIO library should be the speed pin's pwm
+        - speed_percent: an number between 0-100 representing the percent of max speed we go
+    to be calibrated after seeing the car move
+    """
+    cycle = (speed_percent/10) +7.5
+    pwm.ChangeDutyCycle(cycle)
+
